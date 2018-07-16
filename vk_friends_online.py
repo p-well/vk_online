11faf9291fc1fae21edbaaa53b62586242a9817c
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-16 13:33:26 +0300

    Fixed auth exception handling

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -14,22 +14,24 @@ def get_user_password():
 
 
 def fetch_online_friends(login, password):
-    #try:
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-        scope = 2
-    )
-    api = vk.API(session, v='5.78')
-    friends_online_ids = api.friends.getOnline()
-    friends_online_data = api.users.get(
-        user_ids = friends_online_ids,
-        lang = 3
-    )
-    friends_online_data = friends_online_data
-#    except requests.exceptions.RequestException:
-#        friends_online_data = []
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope = 2
+        )
+        api = vk.API(session, v='5.78')
+        friends_online_ids = api.friends.getOnline()
+        friends_online_data = api.users.get(
+            user_ids = friends_online_ids,
+            lang = 3
+        )
+        friends_online_data = friends_online_data
+    except requests.exceptions.RequestException:
+        friends_online_data = None
+    except vk.exceptions.VkAuthError:
+        friends_online_data = []
     return friends_online_data
 
 
@@ -43,12 +45,13 @@ def show_online_friends(friends_online_data):
         ))
 
 
-
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
     friends_online_data = fetch_online_friends(login, password)
     if friends_online_data:
         show_online_friends(friends_online_data)
-    else:
-        print('\nError')
+    elif friends_online_data is None:
+        print('\nConnection error.')
+#    elif not friends_online_data:
+#       print('\nAuthorization error (incorrect password).')
